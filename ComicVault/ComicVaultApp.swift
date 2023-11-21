e4992a130d6ce8d08d4bda032946ff6b1f7a64be
Ayman Tauhid <aymantauhid2@gmail.com>
2023-11-21 11:48:13 -0500

    Initial Commit

---
+++ ComicVault/ComicVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicVaultApp.swift
+//  ComicVault
+//
+//  Created by Ayman Tauhid on 2023-11-21.
+//
+
+import SwiftUI
+
+@main
+struct ComicVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComicVault/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ComicVault
+//
+//  Created by Ayman Tauhid on 2023-11-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
